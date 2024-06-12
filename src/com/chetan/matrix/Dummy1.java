6f3381dfbbc2cdc24c0c299479da9011c233fb60
Chetan <malichetan012@gmail.com>
2024-06-12 13:46:19 +0530

    second new line is added in the dummy class

--- src/com/chetan/matrix/Dummy1.java
+++ src/com/chetan/matrix/Dummy1.java
@@ -3,6 +3,8 @@ package com.chetan.matrix;
 public class Dummy1 {
     public static void main(String[] args) {
         System.out.println("This is my first line");
+
+        System.out.println("This is my second line");
     }
 
 }
